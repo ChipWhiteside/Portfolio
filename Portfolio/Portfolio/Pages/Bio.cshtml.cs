c6ed19520533f68e2a9390c6eaa2fe8607f4aeed
Chip Whiteside <chippy.jwhitesi@gmail.com>
2023-02-13 21:43:33 -0500

    Image Loading
    Fixed the frustrating image local paths

---
+++ Portfolio/Portfolio/Pages/Bio.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class BioModel : PageModel
+    {
+        private readonly ILogger<BioModel> _logger;
+
+        public BioModel(ILogger<BioModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
